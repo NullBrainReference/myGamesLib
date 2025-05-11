8436c4312ceb77e10acd777a7d23a5a62471e3c8
NullBrainReference <skapinzev@gmail.com>
2025-05-11 17:39:12 -0300

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <x-navbar />
+    </header>
+    <main></main>
+
+    <script src="{{ url('js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
