b61da26cde8a458ad5a97616ad622041a5528c14
NullBrainReference <88146490+NullBrainReference@users.noreply.github.com>
2025-05-15 17:16:57 -0300

    user' games many to many

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Game extends Model
 {
@@ -15,4 +16,8 @@ class Game extends Model
     protected $fillable = ['title', 'description', 'img_src']; // Fillable columns
 
     public $incrementing = false; // If game_id is non-incrementing
+
+    public function users(): BelongsToMany {
+        return $this->belongsToMany(User::class, 'game_user');
+    }
 }
