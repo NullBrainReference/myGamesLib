df79faa2635ee1b337f5d158af656d992c55ce3d
NullBrainReference <skapinzev@gmail.com>
2025-05-17 17:59:03 -0300

    titles fix

--- resources/views/games/shop.blade.php
+++ resources/views/games/shop.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ url('css/style.css') }}">
-    <title>Home</title>
+    <title>Shop</title>
 </head>
 <body>
     <header>
