467e18ad31ec3dcb4729085f105c6c3cfa0b963d
NullBrainReference <88146490+NullBrainReference@users.noreply.github.com>
2026-04-01 15:27:42 -0300

    project migration

---
+++ app/Models/Project.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'is_public',
+        'icon_big',
+        'icon_small',
+    ];
+
+    /**
+     * Owners of the project
+     */
+    public function owners()
+    {
+        return $this->belongsToMany(User::class, 'project_owners')
+                    ->withTimestamps();
+    }
+
+    /**
+     * Editors of the project
+     */
+    public function editors()
+    {
+        return $this->belongsToMany(User::class, 'project_editors')
+                    ->withTimestamps();
+    }
+
+    /**
+     * General participants of the project
+     */
+    public function participants()
+    {
+        return $this->belongsToMany(User::class, 'project_participants')
+                    ->withTimestamps();
+    }
+}
